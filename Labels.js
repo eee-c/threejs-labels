94ca14c01e5a91d436f1af11a73d4afd999f64ab
Aaron Curtis <foobarbecue@gmail.com>
2014-04-17 14:02:21 +0900

    Add marker in addition to label text. Now requires jquery... could get rid of this dependancy.

--- Labels.js
+++ Labels.js
@@ -32,48 +32,76 @@ THREE.CanvasRenderer = function(parameters) {
 function Label(object, content, duration) {
 //     probably need to add options object here
   this.object = object;
+  this.content = content;
+  this.showText = true;
+  this.showMarker = true;
   if (duration) this.remove(duration);
-
+  this.updatePosition(scene, camera);
   this.el = this.buildElement();
+  this.marker = this.buildMarker();
   LabelPlugin.add(this);
+}
+
+Label.prototype.buildMarker = function() {
+    spGeom = new THREE.SphereGeometry(0.05,6,6);
+    spMat = new THREE.MeshBasicMaterial();
+    marker = new THREE.Mesh(spGeom, spMat);
+    marker.position = this.position;
+    return marker    
 }
 
 Label.prototype.buildElement = function() {
   var el = document.createElement('div');
   el.classList.add("label3D");
   if(typeof this.content == "string"){
-    el.textContent = this.content;
-    }  else {
-    var inp = document.createElement('input')
-    inp.setAttribute("type","text")
-    inp.setAttribute("value","enter your message")
-    el.appendChild(inp);
-    inp.focus();        
-    }
+        el.textContent = this.content;
+        }  else {
+        var inp = document.createElement('input')
+        inp.setAttribute("type","text")
+        inp.setAttribute("value","enter your message")
+        el.appendChild(inp);
+        inp.focus();        
+  }
   
   document.body.appendChild(el);
   return el;
 };
 
-Label.prototype.render = function(scene, cam) {
-  if (this.object instanceof THREE.Vector3){
-    p3d=this.object.clone()
-  }
-  else{
-    var p3d = this.object.position.clone();
-    p3d.z = p3d.z + this.object.geometry.boundingSphere.radius * Math.sin(cam.rotation.x);
-    p3d.y = p3d.y + this.object.geometry.boundingSphere.radius * Math.cos(cam.rotation.x) * Math.cos(cam.rotation.z);
-    p3d.x = p3d.x - this.object.geometry.boundingSphere.radius * Math.sin(cam.rotation.z) * Math.sin(cam.rotation.y);
-  }
+Label.prototype.updatePosition = function(scene, camera) {
+    if (this.object instanceof THREE.Vector3){
+        this.position=this.object.clone()
+    }
+    else{
+        this.position = this.object.position.clone();
+        this.position.z = this.position.z + this.object.geometry.boundingSphere.radius * Math.sin(camera.rotation.x);
+        this.position.y = this.position.y + this.object.geometry.boundingSphere.radius * Math.cos(camera.rotation.x) * Math.cos(camera.rotation.z);
+        this.position.x = this.position.x - this.object.geometry.boundingSphere.radius * Math.sin(camera.rotation.z) * Math.sin(camera.rotation.y);
+    }
 
-  var projector = new THREE.Projector(),
-      pos = projector.projectVector(p3d, cam),
-      width = window.innerWidth,
-      height = window.innerHeight,
-      w = this.el.offsetWidth,
-      h = this.el.offsetHeight;
-  this.el.style.top = '' + (height/2 - height/2 * pos.y - h - 10) + 'px';
-  this.el.style.left = '' + (width/2 * pos.x + width/2 - w/2) + 'px';
+}
+
+Label.prototype.render = function(scene) {
+  this.updatePosition();
+    var projector = new THREE.Projector(),
+        pos = projector.projectVector(this.position, camera),
+        width = window.innerWidth,
+        height = window.innerHeight,
+        w = this.el.offsetWidth,
+        h = this.el.offsetHeight;
+    this.el.style.top = '' + (height/2 - height/2 * pos.y - h - 10) + 'px';
+    this.el.style.left = '' + (width/2 * pos.x + width/2 - w/2) + 'px';      
+ if (this.showMarker){    
+    scene.add(this.marker)
+  }
+   else {
+       scene.remove(this.marker)
+   }
+ if (this.showText){
+     $(this.el).show();
+ }
+  else{
+    $(this.el).hide();
+  }
 };
 
 Label.prototype.setContent = function(content) {
